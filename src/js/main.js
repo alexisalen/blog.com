3da6ad4d8cfc84a92cb788ec562447c04da97b91
Alexis Walter <alexis.alen@gmail.com>
2019-11-04 09:03:34 +0100

    slider plugin setup

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+        $('.galeria').bxSlider({
+            mode: 'fade',
+            captions: 'true',
+            slidewidth: 1200,
+            responsive: true,
+            pager:true,
+        });
+});
