4183f6e3169d371714c1ee760929e179a6e1e9ae
Alexis Walter <alexis.alen@gmail.com>
2020-01-26 16:19:42 +0100

    local storage login

--- src/js/main.js
+++ src/js/main.js
@@ -68,5 +68,28 @@ $(document).ready(function () {
         theme.attr("href", "src/css/red.css");
     });
 
+    //Login falso
 
+    $("#login form").submit(function(){
+        var form_name = $("#form_name").val();
+
+        localStorage.setItem("form_name", form_name);
+
+    });
+
+    var form_name = localStorage.getItem("form_name");
+
+    if(form_name != null && form_name != "undefined"){
+        var about_parrafo = $("#about p");
+
+        about_parrafo.html("<br><strong>Bienvenido, " +form_name+"</strong>");
+        about_parrafo.append("<a href='#' id='logout'>Cerrar sesion</a>");
+        
+        $("#login").hide();
+
+        $("#logout").click(function(){
+            localStorage.clear();
+            location.reload();
+        });
+    } 
 });
